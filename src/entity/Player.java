d0c7d8a9fe468092a68354b1a3ddcbd8f0462c83
Fernando <falbasossa@.gmail.com>
2025-04-26 01:24:13 +0200

    Cambios en player draw() update() variables en enity spriteCouter,spriteNum para simular el caminar y loop de cambio de sprites

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -5,8 +5,8 @@ package entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-//import java.io.IOException;
-//import javax.imageio.ImageIO;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import main.GamePanel;
 import manager.KeyManager;
 
@@ -19,28 +19,28 @@ public class Player extends Entity{
 		this.gp=gp;
 		this.km=km;
 		setDefaultValues();
-//		getPlayerImage();
+		getPlayerImage();
 	}
 	public void setDefaultValues() {
 		x=100;
 		y=100;
 		speed=5;
-//		direction="down";
+		direction="down";
 	}
-//	public void getPlayerImage() {
-//		try {
-//		up1=ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-//		up2=ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-//		down1=ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-//		down2=ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-//		left1=ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-//		left2=ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-//		right1=ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-//		right2=ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
-//		} catch (IOException e) {
-//			e.getStackTrace();
-//		}
-//	}
+	public void getPlayerImage() {
+		try {
+		up1=ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+		up2=ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+		down1=ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+		down2=ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+		left1=ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+		left2=ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+		right1=ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+		right2=ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+	}
 	public void update() {
 		if (km.up==true) {
 			direction="up";
@@ -63,14 +63,13 @@ public class Player extends Entity{
 		
 		g2.setColor(Color.PINK);
 		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-//		BufferedImage img = null;
-//		switch(direction) {
-//		case"up"->img=up1;
-//		case"down"->img=down1;
-//		case"left"->img=left1;
-//		case"right"->img=right1;
-
+		BufferedImage img = null;
+		switch(direction) {
+		case"up"->img=up1;
+		case"down"->img=down1;
+		case"left"->img=left1;
+		case"right"->img=right1;
 		}
-//		g2.drawImage(img, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(img, x, y, gp.tileSize, gp.tileSize, null);
 	}
 }
