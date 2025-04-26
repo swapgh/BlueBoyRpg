7c9c9f3cbdcdbfe0d5894d60de69878d568970a5
Fernando <falbasossa@.gmail.com>
2025-04-26 02:37:05 +0200

    Pintado automatico de tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -11,25 +14,51 @@ public class TileManager {
 	
 	GamePanel gp;
 	Tile[]tile;
+	int mapTileNum [][];
 	
 	public TileManager(GamePanel gp) {
 		this.gp=gp;
 		
 		tile=new Tile[10];
+		mapTileNum=new int[gp.maxScreenCol][gp.maxScreenRow];
 		getTileImage();
+		loadMap();
 	}
-	
+	public void loadMap() {
+		try {
+			InputStream is = getClass().getResourceAsStream("/map/map0.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			int col=0;
+			int row=0;
+			while (col<gp.maxScreenCol&&row<gp.maxScreenRow) {
+				String line = br.readLine();
+				while (col<gp.maxScreenCol) {
+					String numbers[]=line.split(" ");
+					int num= Integer.parseInt(numbers[col]);
+					mapTileNum[col][row]=num;
+					col++;
+				}
+				if (col==gp.maxScreenCol) {
+					col=0;
+					row++;
+				}
+			
+			}
+			br.close();
+		} catch (Exception e) {
+		}
+	}
 	public void getTileImage() {
 		try {
-			int i=0;
-			tile[i]=new Tile();
-			tile[i].image=ImageIO.read(getClass().getResourceAsStream("/tiles/grass01.png"));
-			i++;
-			tile[i]=new Tile();
-			tile[i].image=ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
-			i++;
-			tile[i]=new Tile();
-			tile[i].image=ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
+		
+			tile[0]=new Tile();
+			tile[0].image=ImageIO.read(getClass().getResourceAsStream("/tiles/grass01.png"));
+	
+			tile[1]=new Tile();
+			tile[1].image=ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+		
+			tile[2]=new Tile();
+			tile[2].image=ImageIO.read(getClass().getResourceAsStream("/tiles/water00.png"));
 
 			
 		} catch (IOException e) {
@@ -45,7 +74,8 @@ public class TileManager {
 		int x=0;
 		int y=0;
 		while(col<gp.maxScreenCol && row<gp.maxScreenRow) {
-			g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+			int tileNum = mapTileNum[col][row];
+			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
 			col++;
 			x+=gp.tileSize;
 			if (col==gp.maxScreenCol) {
